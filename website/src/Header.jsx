484accb6ec2fb4cbe7f648d7cff4d2c94d4bc7b9
PiriCavanagh <pcava13.2@gmail.com>
2024-03-06 20:12:55 +1000

    got vite and tailwind working with the application

---
+++ website/src/Header.jsx
@@ -0,0 +1,15 @@
+function Header() {
+    return  (
+        <header>
+            <nav>
+                <ul className='bg-purple-700'>
+                    <li><a href="#">Festivals</a></li>
+                    <li><a href="#">Trending Updates</a></li>
+                    <li><a href="#">About us</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+
+}
+export default Header
